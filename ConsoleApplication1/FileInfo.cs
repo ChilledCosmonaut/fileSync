a454708d71cb32422514a2f9b96d84078a8bc14b
GoPJo <jonas.schindler@stud-mail.uni-wuerzburg.de>
2022-01-05 01:39:04 +0100

    Traverse folders now recursively while building up a json that ignores .git folders and stores remote link

---
+++ ConsoleApplication1/FileInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    public class FileInfo
+    {
+        public string fileName;
+
+        public DateTime lastTimeEdited;
+    }
+}
---
+++ ConsoleApplication1/FolderInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApplication1
+{
+    public class FolderInfo
+    {
+        public string folderName;
+
+        public List<FolderInfo> subfolderInfos = new List<FolderInfo>();
+
+        public List<FileInfo> fileInfos = new List<FileInfo>();
+
+        public bool projectFolder = false;
+
+        public string gitUrl;
+    }
+}
